e935b71968ee04f7729ee6d07e18f6cb7783ad14
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-05-18 07:47:38 +0700

    feat: validate utils

---
+++ src/utils/validate.ts
@@ -0,0 +1,20 @@
+import { FirebaseError } from '@firebase/util';
+
+export type HasMessage = { message: string };
+
+export const hasMessage = (obj: any): obj is HasMessage =>
+  typeof obj?.message === 'string';
+export const isNullOrUndefined = (obj: unknown): obj is null | undefined =>
+  obj === null || obj === undefined;
+
+export const notEmpty = <T>(obj: T | null | undefined): obj is T =>
+  !isNullOrUndefined(obj);
+
+export const isEmptyString = (str: string | null | undefined): boolean =>
+  isNullOrUndefined(str) || str.trim().length === 0;
+
+export const hasName = (obj: any): obj is { name: string } =>
+  typeof obj?.name === 'string';
+
+export const isFirebaseError = (error: unknown): error is FirebaseError =>
+  hasName(error) && error.name === 'FirebaseError';
