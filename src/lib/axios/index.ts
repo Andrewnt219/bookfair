d82033882ca240903a02be4854240c4e451fef96
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-05-18 09:10:45 +0700

    refactor(lib/axios): delete interceptors

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,17 +1,5 @@
 import Axios from 'axios';
-import { getErrorMessage } from '../../utils';
 
 export const axios = Axios.create({
-  baseURL: '/',
+  baseURL: '/api',
 });
-
-axios.interceptors.response.use(
-  (response) => {
-    return response.data;
-  },
-  (error) => {
-    const message = getErrorMessage(error);
-    console.error({ error: message });
-    return Promise.reject(error);
-  }
-);
