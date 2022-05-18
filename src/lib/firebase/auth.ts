ded491d1380fa939496b261e5990544fa08b16d2
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-05-18 08:01:11 +0700

    refactore: firebase lib exports name

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -1,4 +1,4 @@
 import { getAuth } from 'firebase/auth';
 import { app } from './app';
 
-export const auth = getAuth(app);
+export const firebaseAuth = getAuth(app);
--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -1,4 +1,4 @@
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { app } from './app';
 
-export const db = getFirestore(app);
+export const firestore = getFirestore(app);
