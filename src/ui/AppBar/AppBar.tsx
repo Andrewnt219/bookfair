bbc02af571d923afc6723c0deb983a8bd7782d9a
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-05-24 13:10:06 +0700

    feat(ui): add AppBar

---
+++ src/ui/AppBar/AppBar.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { Icon } from '@iconify/react';
+
+export interface AppBarProps {
+  className?: string;
+}
+
+export const AppBar = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <header className="fixed-bottom bg-light">
+      <Nav variant="pills" as="nav" activeKey={pathname} fill>
+        <Nav.Item>
+          <Link href="/" passHref>
+            <Nav.Link>
+              <Icon icon="bi:house-door-fill" />
+            </Nav.Link>
+          </Link>
+        </Nav.Item>
+
+        <Nav.Item>
+          <Link href="/listing" passHref>
+            <Nav.Link>
+              <Icon icon="bi:basket3-fill" />
+            </Nav.Link>
+          </Link>
+        </Nav.Item>
+      </Nav>
+    </header>
+  );
+};
