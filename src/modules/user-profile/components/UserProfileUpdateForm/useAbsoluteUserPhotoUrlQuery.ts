f6ad729e0b0cdf54b2a33cff583004b6a221bae9
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-05-25 14:50:49 +0700

    fix(modules): don't fetch photo on non-auth

--- src/modules/user-profile/components/UserProfileUpdateForm/useAbsoluteUserPhotoUrlQuery.ts
+++ src/modules/user-profile/components/UserProfileUpdateForm/useAbsoluteUserPhotoUrlQuery.ts
@@ -1,13 +1,16 @@
 import { useQuery } from 'react-query';
+import { firebaseAuth } from '../../../../lib/firebase';
 import { useToastStore } from '../../../../stores';
 import { UserProfileApi } from '../../api';
 
 export const useUserAbsolutePhotoUrlQuery = () => {
   const toastStore = useToastStore();
+  const { currentUser } = firebaseAuth;
 
   return useQuery({
     queryFn: () => UserProfileApi.getUserAbsolutePhotoUrl(),
     queryKey: 'user-photo-url',
+    enabled: Boolean(currentUser),
     onError: (error) => {
       toastStore.error(error);
     },
