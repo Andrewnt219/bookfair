488ad7b9d7760aa907ed0b012e1263a7e574074d
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-06-15 14:04:08 +0700

    feat(user-profile): add home navigation page

--- src/pages/user/me.tsx
+++ src/pages/user/me.tsx
@@ -1,21 +1,48 @@
 import { NextPageWithLayout } from '@bookfair/next';
 import { UserProfileLayout } from '../../layouts';
-import {
-  DeleteUserButton,
-  UserProfileUpdateForm,
-} from '../../modules/user-profile';
+import NextLink, { LinkProps } from 'next/link';
+import { Icon, IconProps } from '@iconify/react';
+
+interface Route extends LinkProps {
+  text: string;
+  iconName: string;
+}
+
+const routes: Route[] = [
+  {
+    href: '/user/profile',
+    text: 'Profile',
+    iconName: 'bi:credit-card-2-front-fill',
+  },
+  {
+    href: '/user/reviews',
+    text: 'Reviews',
+    iconName: 'bi:chat-left-text-fill',
+  },
+  {
+    href: '/user/listings',
+    text: 'Listings',
+    iconName: 'bi:aspect-ratio-fill',
+  },
+];
 
 const UserMePage: NextPageWithLayout = () => {
   return (
-    <>
-      <div className="mt-5 shadow p-5 rounded">
-        <UserProfileUpdateForm />
-      </div>
-
-      <div className="text-center mt-3">
-        <DeleteUserButton />
-      </div>
-    </>
+    <ul className="list-unstyled row">
+      {routes.map(({ text, iconName, ...route }) => (
+        <li key={route.href.toString()} className="col-6 mt-3">
+          <NextLink href="/user/profile">
+            <a
+              className="d-flex flex-column fw-normal align-items-center justify-content-center gap-2 shadow-sm rounded"
+              style={{ aspectRatio: '1/1' }}
+            >
+              <Icon icon={iconName} className="display-1" />
+              {text}
+            </a>
+          </NextLink>
+        </li>
+      ))}
+    </ul>
   );
 };
 
