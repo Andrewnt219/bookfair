36f77b0d792f190155c5dbe7928337764eb554da
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-06-19 11:44:26 +0700

    feat(modules): add delete listing

--- src/modules/listing/components/ListingList/ListingListItem/index.tsx
+++ src/modules/listing/components/ListingList/ListingListItem/index.tsx
@@ -3,7 +3,8 @@ import { WithQueryData } from '../../../../../ui/WithQueryData';
 import { useListingPhotoSources } from '../../../api';
 import { DbListing } from '../../../types';
 import NextLink from 'next/link';
-import { Button, Card, Stack } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { DeleteListingButton } from '../../DeleteListingButton';
 
 export interface ListingListItemProps {
   listing: DbListing;
@@ -13,38 +14,39 @@ export const ListingListItem = ({ listing }: ListingListItemProps) => {
   const photoSourcesQuery = useListingPhotoSources({ photos: listing.photos });
 
   return (
-    <Card as="article">
-      <WithQueryData query={photoSourcesQuery}>
-        {(sources) => (
-          <Card.Img
-            style={{ objectFit: 'cover', aspectRatio: '1/1' }}
-            variant="top"
-            src={sources[0]}
-          />
-        )}
-      </WithQueryData>
+    <>
+      <Card as="article">
+        <WithQueryData query={photoSourcesQuery}>
+          {(sources) => (
+            <Card.Img
+              style={{ objectFit: 'cover', aspectRatio: '1/1' }}
+              variant="top"
+              src={sources[0]}
+            />
+          )}
+        </WithQueryData>
 
-      <Card.Body>
-        <Card.Title>{listing.title}</Card.Title>
-        <Card.Text>
-          <dl>
-            <dt>View count</dt>
-            <dd> {listing.viewCount}</dd>
-            <dt>Description</dt>
-            <dd> {listing.description}</dd>
-            <dt>Price</dt>
-            <dd> {listing.price}</dd>
-          </dl>
+        <Card.Body>
+          <Card.Title>{listing.title}</Card.Title>
+          <Card.Text>
+            <dl>
+              <dt>View count</dt>
+              <dd> {listing.viewCount}</dd>
+              <dt>Description</dt>
+              <dd> {listing.description}</dd>
+              <dt>Price</dt>
+              <dd> {listing.price}</dd>
+            </dl>
 
-          <div className="d-flex gap-1 justify-content-end">
-            <Button variant="danger">Delete</Button>
-
-            <NextLink href={`/listing/${listing.id}/edit`}>
-              <a className="btn btn-primary">Edit</a>
-            </NextLink>
-          </div>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+            <div className="d-flex gap-1 justify-content-end">
+              <DeleteListingButton listingId={listing.id} />
+              <NextLink href={`/listing/${listing.id}/edit`}>
+                <a className="btn btn-primary">Edit</a>
+              </NextLink>
+            </div>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
