f8e7589388e0282e68a275408863cb97f80df4f8
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-06-19 12:10:13 +0700

    feat(modules): add ui for listing limit

--- src/pages/user/listings.tsx
+++ src/pages/user/listings.tsx
@@ -6,31 +6,53 @@ import { useAuthUserStore } from '../../stores';
 import { WithQueryData } from '../../ui/WithQueryData';
 import { useAuthRoute } from '../../utils/useAuthRoute';
 import NextLink from 'next/link';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import { Icon } from '@iconify/react';
+import { useDbUserQuery } from '../../modules/user-profile';
 
 const UserListingsPage: NextPageWithLayout = () => {
   useAuthRoute();
   const { authUser } = useAuthUserStore();
   const getListingsQuery = useGetListings({ userId: authUser?.uid });
+  const userProfileQuery = useDbUserQuery(authUser?.uid);
 
   return (
-    <Container fluid as="section" className="col-lg-4">
-      <h1>My listings</h1>
+    <WithQueryData query={getListingsQuery}>
+      {(listings) => (
+        <WithQueryData query={userProfileQuery}>
+          {(profile) => (
+            <Container fluid as="section" className="col-lg-4">
+              <h1>My listings</h1>
+              <p className="text-muted">
+                {listings.length}/{profile.listingLimit} listings used
+              </p>
 
-      <NextLink href="/listing/new">
-        <a className="btn btn-primary d-inline-flex gap-2 align-items-center mt-5">
-          Create new listing
-          <Icon icon="bi:plus-circle-fill" width={20} />
-        </a>
-      </NextLink>
+              <div className="mt-5">
+                {listings.length === profile.listingLimit ? (
+                  <NextLink href="/listing/purchase">
+                    <a className="btn btn-primary d-inline-flex gap-2 align-items-center">
+                      Purchase more listings
+                      <Icon icon="bi:cart-plus-fill" width={20} />
+                    </a>
+                  </NextLink>
+                ) : (
+                  <NextLink href="/listing/new">
+                    <a className="btn btn-primary d-inline-flex gap-2 align-items-center">
+                      Create new listing
+                      <Icon icon="bi:plus-circle-fill" width={20} />
+                    </a>
+                  </NextLink>
+                )}
+              </div>
 
-      <div className="mt-4">
-        <WithQueryData query={getListingsQuery}>
-          {(listings) => <ListingList listings={listings} />}
+              <div className="mt-4">
+                <ListingList listings={listings} />
+              </div>
+            </Container>
+          )}
         </WithQueryData>
-      </div>
-    </Container>
+      )}
+    </WithQueryData>
   );
 };
 
