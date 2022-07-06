4f2959164e50fac92dd8e246ecb1268d4a58444b
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-07-06 09:03:58 -0400

    fix(modules): optimize CLS of listing cards

--- src/modules/listing/components/ListingList/ListingListItem/index.tsx
+++ src/modules/listing/components/ListingList/ListingListItem/index.tsx
@@ -18,83 +18,83 @@ export const ListingListItem = ({ listing }: ListingListItemProps) => {
   const photoSourcesQuery = useListingPhotoSources({ photos: listing.photos });
 
   return (
-    <>
-      <Card as="article">
-        <WithQueryData query={photoSourcesQuery}>
-          {(sources) => (
-            <Card.Img
-              style={{ objectFit: 'cover', aspectRatio: '1/1' }}
-              variant="top"
-              src={sources[0]}
-            />
-          )}
-        </WithQueryData>
+    <WithQueryData query={photoSourcesQuery}>
+      {(sources) => (
+        <Card as="article">
+          <Card.Img
+            style={{ objectFit: 'cover', aspectRatio: '1/1' }}
+            variant="top"
+            src={sources[0]}
+          />
 
-        <Card.Body>
-          <Card.Title>{listing.title}</Card.Title>
+          <Card.Body>
+            <Card.Title>{listing.title}</Card.Title>
 
-          <Card.Text as="div">
-            <ul className="list-unstyled d-flex gap-1 flex-wrap">
-              {listing.tags.map((tag, index) => (
-                <li key={index}>
-                  <Badge bg="light" text="dark">
-                    {tag}
-                  </Badge>
-                </li>
-              ))}
-            </ul>
-            <p className="mt-3"> {listing.description}</p>
-            <dl
-              style={{
-                display: 'grid',
-                gridTemplateColumns: 'min-content max-content',
-                gap: '0.5rem 1rem',
-              }}
-            >
-              {listing.promote && (
-                <>
-                  <dt className="text-danger">
-                    <Icon
-                      icon="bi:chevron-double-up"
-                      aria-label="Promotion's end date"
-                    />
-                  </dt>
-                  <dd>
-                    {dayjs(listing.promote).format('MMMM DD, YYYY hh:mm:ss A')}
-                  </dd>
-                </>
-              )}
-              <dt>
-                <Icon
-                  icon="bi:aspect-ratio-fill"
-                  aria-label="Listing's course"
-                />
-              </dt>
-              <dd>{listing.course}</dd>
+            <Card.Text as="div">
+              <ul className="list-unstyled d-flex gap-1 flex-wrap">
+                {listing.tags.map((tag, index) => (
+                  <li key={index}>
+                    <Badge bg="light" text="dark">
+                      {tag}
+                    </Badge>
+                  </li>
+                ))}
+              </ul>
+              <p className="mt-3"> {listing.description}</p>
+              <dl
+                style={{
+                  display: 'grid',
+                  gridTemplateColumns: 'min-content max-content',
+                  gap: '0.5rem 1rem',
+                }}
+              >
+                {listing.promote && (
+                  <>
+                    <dt className="text-danger">
+                      <Icon
+                        icon="bi:chevron-double-up"
+                        aria-label="Promotion's end date"
+                      />
+                    </dt>
+                    <dd>
+                      {dayjs(listing.promote).format(
+                        'MMMM DD, YYYY hh:mm:ss A'
+                      )}
+                    </dd>
+                  </>
+                )}
+                <dt>
+                  <Icon
+                    icon="bi:aspect-ratio-fill"
+                    aria-label="Listing's course"
+                  />
+                </dt>
+                <dd>{listing.course}</dd>
 
-              <dt>
-                <Icon icon="bi:cart-fill" aria-label="Listing's Price" />
-              </dt>
-              <dd>{formatCurrency(listing.price)}</dd>
+                <dt>
+                  <Icon icon="bi:cart-fill" aria-label="Listing's Price" />
+                </dt>
+                <dd>{formatCurrency(listing.price)}</dd>
 
-              <dt>
-                <Icon icon="bi:eye-fill" aria-label="Listing's view count" />
-              </dt>
-              <dd>{listing.viewCount}</dd>
-            </dl>
+                <dt>
+                  <Icon icon="bi:eye-fill" aria-label="Listing's view count" />
+                </dt>
+                <dd>{listing.viewCount}</dd>
+              </dl>
 
-            <div className="d-flex gap-1 justify-content-end">
-              {!listing.promote && (
-                <PromoteListingButton listingId={listing.id} />
-              )}
-              <DeleteListingButton listingId={listing.id} />
-              <NextLink href={`/listing/${listing.id}/edit`}>
-                <a className="btn btn-primary">Edit</a>
-              </NextLink>
-            </div>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </>
+              <div className="d-flex gap-1 justify-content-end">
+                {!listing.promote && (
+                  <PromoteListingButton listingId={listing.id} />
+                )}
+                <DeleteListingButton listingId={listing.id} />
+                <NextLink href={`/listing/${listing.id}/edit`}>
+                  <a className="btn btn-primary">Edit</a>
+                </NextLink>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+    </WithQueryData>
   );
 };
