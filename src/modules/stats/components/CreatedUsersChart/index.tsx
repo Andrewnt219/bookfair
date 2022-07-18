4ed043a12dfeb83c5662ca6f0e7a8d47ae6b6f87
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-07-18 15:13:45 -0400

    feat(modules): add ui for created users chart

---
+++ src/modules/stats/components/CreatedUsersChart/index.tsx
@@ -0,0 +1,61 @@
+import { DayjsAble } from '@bookfair/common';
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { WithQueryData } from '../../../../ui';
+import { useCreatedUsersStats } from '../../data/useCreatedUsersStats';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+export interface CreatedUsersChartProps {
+  startDate: DayjsAble;
+  endDate: DayjsAble;
+}
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const CreatedUsersChart = (props: CreatedUsersChartProps) => {
+  const statsQuery = useCreatedUsersStats({
+    endDate: props.endDate.toString(),
+    startDate: props.startDate.toString(),
+  });
+
+  return (
+    <article>
+      <WithQueryData query={statsQuery}>
+        {(stats) => (
+          <Line
+            data={{
+              labels: stats.labels,
+              datasets: stats.datasets,
+            }}
+            options={{
+              responsive: true,
+              plugins: {
+                legend: {
+                  position: 'top' as const,
+                },
+              },
+            }}
+          />
+        )}
+      </WithQueryData>
+    </article>
+  );
+};
---
+++ src/modules/stats/routes/AdminStatsUsersReportRoute.tsx
@@ -0,0 +1,39 @@
+import dayjs from 'dayjs';
+import React from 'react';
+import { Container, Stack } from 'react-bootstrap';
+import { BackButton } from '../../../ui';
+import { CreatedUsersChart } from '../components/CreatedUsersChart';
+
+export interface AdminStatsUsersReportProps {
+  startDate: string;
+  endDate: string;
+}
+
+export const AdminStatsUsersReportRoute = (
+  props: AdminStatsUsersReportProps
+) => {
+  const uiDateRange = (
+    <p className="text-muted">
+      from {dayjs(props.startDate).format('MMM DD, YYYY')} to{' '}
+      {dayjs(props.endDate).format('MMM DD, YYYY')}{' '}
+    </p>
+  );
+  return (
+    <Container as="section" fluid className="col-lg-4">
+      <BackButton />
+
+      <Stack gap={3}>
+        <section>
+          <h2 className="h2 mt-3">Created users</h2>
+          {uiDateRange}
+          <div className="p-3 shadow rounded ">
+            <CreatedUsersChart
+              startDate={props.startDate}
+              endDate={props.endDate}
+            />
+          </div>
+        </section>
+      </Stack>
+    </Container>
+  );
+};
