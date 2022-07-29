677ad35b42b9f47a41a59925fe801987967901ff
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-07-29 12:47:21 -0400

    feat: only allow seneca email

--- src/schemas/emailSchema.ts
+++ src/schemas/emailSchema.ts
@@ -3,4 +3,7 @@ import z from 'zod';
 export const emailSchema = z
   .string()
   .email('Invalid email format')
-  .min(1, 'Email is required');
+  .min(1, 'Email is required')
+  .refine((email) => email.endsWith('myseneca.ca'), {
+    message: 'Must be a @myseneca.ca email',
+  });
