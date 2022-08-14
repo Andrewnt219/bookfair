bb5bd918f94c40c6f8701977e60694b4162bf7d5
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-08-14 13:51:20 -0400

    feat: add logo and favicon

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
 import React, { ReactNode } from 'react';
 import { Container } from 'react-bootstrap';
 import { AppBar, BackButton } from '../ui';
+import logo from '../assets/logo.png';
+import Link from 'next/link';
 
 export interface DefaultLayoutProps {
   children: ReactNode;
@@ -11,7 +14,21 @@ export const RootLayout = (props: DefaultLayoutProps) => {
     <Container fluid>
       <main className="mt-5 pb-5">
         <Container as="section" fluid className="col-lg-4">
-          <BackButton />
+          <Container className="d-flex justify-content-between align-items-center">
+            <BackButton />
+
+            <Link href="/listing">
+              <a style={{ width: 100, aspectRatio: '1/1' }}>
+                <Image
+                  src={logo}
+                  width={500}
+                  height={500}
+                  alt=""
+                  layout="responsive"
+                />
+              </a>
+            </Link>
+          </Container>
 
           <div className="mt-3">{props.children}</div>
         </Container>
